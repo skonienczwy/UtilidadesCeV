c9cd629bf90ecc34f9cb2b9eb1268eb522c3d925
skonienczwy <skonienczwy@hotmail.com>
2019-07-20 00:06:28 +0200

    Modulos

---
+++ Dado/moeda86V2.py
@@ -0,0 +1,30 @@
+def leiadinheiro(msg):
+    valido = False
+    while not  valido:
+        entrada = str(input(msg)).replace(',', '.').strip()
+        if entrada.isalpha() or entrada =='':
+            print(f'\033[0;31mERRO: \"{entrada}\" é um preço inválido!\033[m')
+        else:
+            valido = True
+            return float(entrada)
+
+
+
+def leiaInt(msg):
+    ok = False
+    valor = 0
+    while True:
+        n = str(input(msg))
+        if n.isnumeric():
+            valor = int(n)
+            ok = True
+        else:
+            print(f'\033[0;31mERRO: \"{entrada}\" é um preço inválido!\033[m')
+        if ok:
+            break
+    return valor
+
+
+
+
+
